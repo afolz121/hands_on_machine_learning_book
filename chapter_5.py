1c97157682fcdc63819bfb79e5c4c1034f5cbeb8
afolz121 <50084625+afolz121@users.noreply.github.com>
2021-10-18 16:40:42 -0500

    add confusion matrix

--- chapter_5.py
+++ chapter_5.py
@@ -5,7 +5,7 @@ from sklearn import datasets
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
@@ -15,7 +15,7 @@ iris = datasets.load_iris()
 x = iris['data'][:, (2,3)]
 y = (iris['target'] == 2).astype(np.float64)
 
-x_train, x_test, y_train, y_test = train_test_split(x,y, train_size= .9, random_state = 1232)
+x_train, x_test, y_train, y_test = train_test_split(x,y, train_size= .6, random_state = 1232)
 
 
 
@@ -30,7 +30,7 @@ svm_clf.fit(x_train,y_train)
 # %%
 
 preds = svm_clf.predict(x_test)
-
+print(confusion_matrix(y_test, preds))
 print(classification_report(y_test,preds))
 # %%
 

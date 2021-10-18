9da2e269cc7f5eddb6fc2cacac61a3d6c886b3a3
afolz121 <50084625+afolz121@users.noreply.github.com>
2021-10-18 16:31:52 -0500

    finish chapter 5

---
+++ chapter_5.py
@@ -0,0 +1,37 @@
+#%%
+
+import numpy as np
+from sklearn import datasets
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+# %%
+
+iris = datasets.load_iris()
+x = iris['data'][:, (2,3)]
+y = (iris['target'] == 2).astype(np.float64)
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, train_size= .9, random_state = 1232)
+
+
+
+#%%
+
+svm_clf = Pipeline([
+    ("scaler", StandardScaler()),
+    ("linear_csv", LinearSVC(C=15, loss = 'hinge'))
+])
+
+svm_clf.fit(x_train,y_train)
+# %%
+
+preds = svm_clf.predict(x_test)
+
+print(classification_report(y_test,preds))
+# %%
+
+
